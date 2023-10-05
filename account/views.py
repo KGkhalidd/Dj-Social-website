b0b32ae404efc8aa893403161c101d4ca551f112
khaled <khaledgamal1345@gmail.com>
2023-10-05 23:41:52 +0300

    used messages framework to display messages to users

--- account/views.py
+++ account/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Profile
+from django.contrib import messages
 # Create your views here.
 
 @login_required
@@ -14,6 +15,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
