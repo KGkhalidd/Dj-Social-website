159a05c7dde308cbce6051911b2e9cf0bb4a1eed
khaled <khaledgamal1345@gmail.com>
2023-10-05 22:19:38 +0300

    edit the edit view

--- account/views.py
+++ account/views.py
@@ -16,7 +16,7 @@ def edit(request):
             profile_form.save()
     else:
         user_form = UserEditForm(instance=request.user)
-        profile_form = ProfileEditForm(instance=request.profile)
+        profile_form = ProfileEditForm(instance=request.user.profile)
 
     return render(request, 'account/edit.html', {'user_form':user_form, 'profile_form':profile_form})
 
