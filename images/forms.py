db267397d7dc931d701c9d46b148ee7a519857da
khaled <khaledgamal1345@gmail.com>
2023-10-13 00:49:33 +0300

    register image model to website, made a form to take data from users about the image and override the save method of that ModelForm

---
+++ images/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Image
+
+class ImageCreateForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ['title', 'url', 'description']
+        widgets = {
+            'url': forms.HiddenInput,
+        }
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extensions = ['jpg', 'jpeg', 'png']
+        extension = url.rsplit('.',1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError('The given URL doesn\'t match valid extensions. ')
+        return url
