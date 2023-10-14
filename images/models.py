02a6ec83eb5f455d0d68a208bdc5f3ed5e1dfb64
khaled <khaledgamal1345@gmail.com>
2023-10-15 02:17:51 +0300

    added bookmarklet launcher

--- images/models.py
+++ images/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.urls import reverse
 # Create your models here.
 
 class Image(models.Model):
@@ -26,3 +27,6 @@ class Image(models.Model):
 
     def __str__(self):
         return self.title
+    
+    # def get_absolute_url(self):
+    #     return reverse("images:create", args=[self.id])
