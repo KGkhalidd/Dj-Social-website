466af591c55230af6378e590a6c7bbd532d2a175
khaled <khaledgamal1345@gmail.com>
2023-10-17 11:11:49 +0300

    added like/unlike functionality

--- images/views.py
+++ images/views.py
@@ -3,7 +3,27 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
 from .models import Image
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 # Create your views here.
+
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except Image.DoesNotExist:
+            pass
+    return JsonResponse({'status': 'error'})
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id , slug=slug)
