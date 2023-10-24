9b55b1b2b60cc899f183ee35368ac93fdde9efb2
khaled <khaledgamal1345@gmail.com>
2023-10-24 06:31:12 +0300

    fininshed activity stream

--- account/views.py
+++ account/views.py
@@ -10,6 +10,8 @@ from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from .models import Contact
+from actions.utils import create_action
+from actions.models import Action
 # Create your views here.
 
 @require_POST
@@ -22,6 +24,7 @@ def user_follow(request):
             user = User.objects.get(id=user_id)
             if action == 'follow':
                 Contact.objects.get_or_create( user_from=request.user, user_to=user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter( user_from=request.user, user_to=user).delete()
             return JsonResponse({'status':'ok'})
@@ -67,6 +70,7 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request, 'account/register_done.html', {'new_user':new_user})
 
     else:
@@ -75,7 +79,12 @@ def register(request):
 
 @login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id', flat=True)
+    if following_ids:
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions.select_related('user', 'user__profile').prefetch_related('target')[:10]
+    return render(request, 'account/dashboard.html', {'section': 'dashboard', 'actions': actions})
 
 
 #not used
